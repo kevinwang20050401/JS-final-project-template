a8b3b94c0eb13c6ea26b3ce3a212ae5fb385812f
kevinwang20050401 <kevin20050401@gmail.com>
2017-02-04 20:32:43 +0800

    bossmove

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
+var FPS = 60;
 var bgImg = document.createElement("img");
 var badguy = document.createElement("img");
-var tower = document.createElement("img");
-var castle = document.createElement("img");
+var towerimg = document.createElement("img");
+var castleimg = document.createElement("img");
 // 設定這個元素的要顯示的圖片
 bgImg.src = "images/ground.png";
 badguy.src = "images/rukia.gif";
-tower.src = "images/tower-btn.png";
-castle.src = "images/tower.png";
+towerimg.src = "images/tower-btn.png";
+castleimg.src = "images/tower.png";
 // 找出網頁中的 canvas 元素
 var canvas = document.getElementById("game-canvas");
 
@@ -14,23 +15,36 @@ var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
 function draw(){
+	enemy.move();
 	// 將背景圖片畫在 canvas 上的 (0,0) 位置
   ctx.drawImage(bgImg,0,0);
   ctx.drawImage(badguy,enemy.x,enemy.y);
-  ctx.drawImage(tower,576,416,64,64);
+  ctx.drawImage(towerimg,576,416,64,64);
   if (isBuilding==true){
-  ctx.drawImage(castle,cursor.x,cursor.y);
+  ctx.drawImage(castleimg,cursor.x-cursor.x%32,cursor.y-cursor.y%32);
+  }else{
+  	ctx.drawImage(castleimg,tower.x,tower.y);
   }
 }
 // 執行 draw 函式
-setInterval(draw,16);
+setInterval(draw,1000/FPS);
 var enemy={
 	x:64,
-	y:382
+	y:382,
+	speedX:0,
+	speedY:-64,
+	move:function(){
+		this.x=this.x+this.speedX/FPS;
+		this.y=this.y+this.speedY/FPS;
+	}
 };
 var cursor={
 	x:100,
 	y:200
+};
+var tower={
+  x:0,
+  y:0
 };
 $("#game-canvas").on("mousemove",mousemove);
 function mousemove(event){
@@ -43,6 +57,10 @@ function mouseclick(){
 	if(cursor.x>576&&cursor.y>416){
       isBuilding=true;
   }else{
-  	  isBuilding=false;
+  	 if(isBuilding==true){
+  	 	tower.x=cursor.x-cursor.x%32;
+  	 	tower.y=cursor.y-cursor.y%32;
+     }
+  	 	isBuilding=false
   }
 }
